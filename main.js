28e5d783193dadea82cd91391fe16ca563f4234a
Herry Wang <herry@herry-desktop>
2018-04-22 00:42:38 +0800

    fix timing dump all valid links

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ async function run() {
     // const TABLE_SELECTOR = '#siteTable > div:nth-child(odd)';
     const TABLE_SELECTOR = 'thing';
     const GAME_THREAD_SELECTOR = '#ID > div:nth-child(4) > div:nth-child(1) > p:nth-child(1) > span:nth-child(2)';
-    const GAME_TREAD_LINK = '#ID > div:nth-child(4) > div:nth-child(1) > p:nth-child(1) > a:nth-child(1)'
+    const GAME_THREAD_LINK = '#ID > div:nth-child(4) > div:nth-child(1) > p:nth-child(1) > a:nth-child(1)'
     let id_list = await page.evaluate((sel) => {
         let len = document.getElementsByClassName(sel).length;
         let id_list = [];
@@ -20,59 +20,59 @@ async function run() {
         return id_list;
     }, TABLE_SELECTOR);
     console.log('total topics#', id_list.length);
-    let current_id = '';
-    let myurl = '';
+    let id_link_list = [];
     for (let i = 0; i < id_list.length; i++) {
         let game_thread_s = GAME_THREAD_SELECTOR.replace('ID', id_list[i]);
-        let game_thread_link_s = GAME_TREAD_LINK.replace('ID', id_list[i]);
+        let game_thread_link_s = GAME_THREAD_LINK.replace('ID', id_list[i]);
         let is_game_thread = await page.evaluate((sel) => {
             let myobj = document.querySelector(sel);
             if (myobj) {
                 if ("Game Thread" === myobj.getAttribute('title')) {
+                // if ("Info Thread" === myobj.getAttribute('title')) {
                     return true;
                 }
             }
         }, game_thread_s);
 
         if (is_game_thread) {
-            myurl = await page.evaluate((sel) => {
+            const myurl = await page.evaluate((sel) => {
                 let myobj = document.querySelector(sel);
                 if (myobj) {
                     return myobj.getAttribute('href');
                 }
             }, game_thread_link_s);
             console.log(myurl);
-            current_id = id_list[i].replace('thing', '');
+            const current_id = id_list[i].replace('thing', '');
             console.log(current_id);
-            break;
+            id_link_list.push({ id: current_id, url: myurl });
         }
     }
 
-    if (myurl !== '') {
-        console.log("going to ", myurl);
-        await page.goto('https://www.reddit.com' + myurl);
+    let results = id_link_list.map(async (item) => {
+        console.log("going to ", item.url);
+        await page.goto('https://www.reddit.com' + item.url);
         // await page.waitForNavigation();
-        let URL_TABLE_SELECTOR = '#siteTable' + current_id + ' > div:nth-child(1) > div:nth-child(2) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(2) > a';
+        // let URL_TABLE_SELECTOR = '#siteTable' + item.id + ' > div:nth-child(1) > div:nth-child(2) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > table:nth-child(2) > tbody:nth-child(2) > tr:nth-child(1) > td:nth-child(2) > a';
+        let URL_TABLE_SELECTOR = 'table>tbody>tr>td>a';
+        
         const streamer_links = await page.evaluate((sel) => {
             const urllist = document.querySelectorAll(sel);
             const urls = [...urllist];
             return urls.map(link => link.href);
         }, URL_TABLE_SELECTOR);
         console.log("links", streamer_links);
-        if (streamer_links[0]) {
-            const embed_player_url = streamer_links[0].replace('watch', 'embed');
-            console.log('going to player ', embed_player_url);
-            await page.goto(embed_player_url);
-            await page.evaluate((sel) => {
-                const script_string = document.querySelector(sel);
-                console.log(script_string);
-            }, 'body > script:nth-child(3)');
-        }
-        //we only care about the first link
-
-    }
+        // if (streamer_links[0]) {
+        //     const embed_player_url = streamer_links[0].replace('watch', 'embed');
+        //     console.log('going to player ', embed_player_url);
+        //     await page.goto(embed_player_url);
+        //     await page.evaluate((sel) => {
+        //         const script_string = document.querySelector(sel);
+        //         console.log(script_string);
+        //     }, 'body > script:nth-child(3)');
+        // }
+    });
+    Promise.all(results).then((completed) => console.log("DONE"));
     // await page.waitForNavigation();
-    browser.close();
+    // browser.close();
 }
-
 run();
--- taobao1.js
+++ taobao1.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 async function run() {
     const browser = await puppeteer.launch({
         headless: false,
-        args: ['--user-agent=Mozilla/5.0 (Unknown; Linux) AppleWebKit/538.1 (KHTML, like Gecko) Chrome/v1.0.0 Safari/538.1']
+        args: ['--user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/538.1 (KHTML, like Gecko) Chrome/65.0.3325 Safari/538.1']
     });
     const page = await browser.newPage();
     // await page.setJavaScriptEnabled(false);
@@ -17,12 +17,14 @@ async function run() {
     //       console.log(await response.text());
     //     }
     //   });
-    const test_url = 'https://detail.tmall.com/item.htm?id=523957302031';
+    const test_url = 'https://detail.tmall.com/item.htm?id=538964820353';
     await page.goto(test_url);
-    await page.waitFor(5 * 1000);
+    console.info("Cookies:", await page.cookies());
+    // await page.waitFor(5 * 1000);
+    await page.waitForSelector("#J_TabBar > li:nth-child(2) > a");
     await page.click('#J_TabBar > li:nth-child(2) > a');
-    await page.waitFor(5 * 1000);
-
+    await page.waitForSelector('.rate-grid > table:nth-child(1)');
+    // await page.waitFor(3 * 1000);
     const URL_TABLE_SELECTOR = '.rate-grid > table:nth-child(1) > tbody > tr > td > div:nth-child(1)'
     const comments = await page.evaluate((sel) => {
         const urllist = document.querySelectorAll(sel);
